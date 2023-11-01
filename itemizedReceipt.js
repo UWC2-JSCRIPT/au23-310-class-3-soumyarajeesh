9eb00abc7a2d8f6bc360d0923908d4220262f331
Soumya Rajeesh <soumyatk@uw.edu>
2023-10-31 17:25:48 -0700

    spaceship

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -3,28 +3,48 @@
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
 
-
 const logReceipt = (...items)=>{
-let total=0;
-items.forEach((items) =>{
-  total+= items.price;
-  console.log(items.descr,'- $',items.price);
-})
-console.log('Total - $',total);
-}
-const Burrito = {descr:'Burrito',price:5.99};
-const Chips = {descr:'Chips & Salsa',price:2.99};
-const Sprite = {descr:'Sprite',price:1.99};
+// Initialize subtotal and tax variables
+  let subtotal = 0; //Declare Subtotal here
+  let total = 0; // Declare total here
+  let tax = 0; // Declare tax here
+  const taxRate = 0.08; // 8% tax rate (you can adjust this)
 
+  items.forEach((items) =>{
+    subtotal+= items.price;
+    console.log(items.descr,'- $'+ items.price);
+  })
 
-// Check
-logReceipt(
-  { descr: 'Burrito', price: 5.99 },
-  { descr: 'Chips & Salsa', price: 2.99 },
-  { descr: 'Sprite', price: 1.99 }
-);
-// should log something like:
+//Calculate the tax amount
+  tax = subtotal * taxRate;
+
+// Calculate the total including tax
+  total = subtotal + tax;
+
+// Log subtotal, tax, and total
+  console.log('Subtotal- $' + subtotal.toFixed(2));
+  console.log('Tax (8%)- $' + tax.toFixed(2));
+  console.log('Total- $' + total.toFixed(2));
+  }
+
+  const Burrito = {descr:'Burrito',price:5.99};
+  const Chips = {descr:'Chips & Salsa',price:2.99};
+  const Sprite = {descr:'Sprite',price:1.99};
+
+// Check 
+  logReceipt(
+    { descr: 'Burrito', price: 5.99 },
+    { descr: 'Chips & Salsa', price: 2.99 },
+    { descr: 'Sprite', price: 1.99 }
+  );
+
+// Should log something like:
 // Burrito - $5.99
 // Chips & Salsa - $2.99
 // Sprite - $1.99
-// Total - $10.97
+// Subtotal - $10.97
+// Tax- $0.88
+// Total- $11.85
+
+
+
